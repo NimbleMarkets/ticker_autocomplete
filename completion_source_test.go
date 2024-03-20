6e6b4ad6af305e0d7c876c7032808e1272fe498d
Kevin Miller <kevin@neomantra.net>
2024-03-20 09:39:39 -0500

    add completion source and tests

---
+++ completion_source_test.go
@@ -0,0 +1,98 @@
+package ticker_autocomplete
+
+import (
+	"errors"
+	"math"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewCompletionSource(t *testing.T) {
+	expected := &MockCompleter{}
+	factory := func() (Completer, error) {
+		time.Sleep(1 * time.Millisecond)
+		return expected, nil
+	}
+
+	cs := NewCompletionSource(factory)
+	assert.NotNil(t, cs)
+
+	c := cs.GetCompleter()
+	assert.Nil(t, c, "GetCompleter should return nil before the first refresh is complete")
+
+	time.Sleep(10 * time.Millisecond)
+	c = cs.GetCompleter()
+	assert.Same(t, expected, c, "GetCompleter should return the completer after the first refresh is complete")
+}
+
+func TestRefresh(t *testing.T) {
+	expected := &MockCompleter{}
+	count := 0
+	factory := func() (Completer, error) {
+		count++
+		time.Sleep(1 * time.Millisecond)
+		return expected, nil
+	}
+
+	cs := NewCompletionSource(factory)
+	assert.Equal(t, 0, count, "Factory should not be called until the first refresh completes")
+
+	time.Sleep(10 * time.Millisecond)
+	assert.Equal(t, 1, count, "Factory should have been called once during initialization")
+
+	err := cs.Refresh()
+	assert.Nil(t, err, "Refresh should not return an error")
+	assert.Equal(t, 2, count, "Refresh invokes the factory")
+}
+
+func TestAutoRefresh(t *testing.T) {
+	expected := &MockCompleter{}
+	count := 0
+	factory := func() (Completer, error) {
+		count++
+		return expected, nil
+	}
+
+	cs := newCompletionSource(factory, 1*time.Millisecond, 1*time.Millisecond)
+
+	time.Sleep(10 * time.Millisecond)
+	assert.Greater(t, count, 1, "Factory be refreshed many times in 10ms")
+
+	c := cs.GetCompleter()
+	assert.Same(t, expected, c)
+	assert.Nil(t, cs.LastError)
+}
+
+func TestRefresh_Retry(t *testing.T) {
+	expected := &MockCompleter{}
+	count := 0
+	err := errors.New("expected error")
+	factory := func() (Completer, error) {
+		count++
+		return expected, err
+	}
+
+	cs := newCompletionSource(factory, 1*time.Minute, 1*time.Nanosecond)
+
+	time.Sleep(100 * time.Millisecond)
+	assert.Greater(t, count, 1, "Factory be retried many times")
+
+	c := cs.GetCompleter()
+	assert.Same(t, err, cs.LastError)
+	assert.Nil(t, c)
+}
+
+type MockCompleter struct {
+	Results []Completion
+}
+
+func (c *MockCompleter) GetCompletions(prompt string, limit int) []Completion {
+	var results []Completion
+	if limit < 0 {
+		limit = math.MaxInt32
+	}
+
+	return results[:limit]
+}
--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/containerd/console v1.0.4 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
@@ -25,9 +26,12 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/term v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
